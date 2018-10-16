87d7d2a54ec56a29300e234566b5fda0f4c2359e
Apple <praveen@apples-macbook-pro.local>
2018-10-16 11:50:06 +0530

    Initial Commit

---
+++ PopUpStyleView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PopUpStyleView
+//
+//  Created by praveen on 10/16/18.
+//  Copyright © 2018 DemoApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
